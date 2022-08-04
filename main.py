38853a66ae02826c66ceb0a561a4db16613dab93
NikGlad <90272508+NikGlad@users.noreply.github.com>
2022-08-04 23:26:43 +0300

    comments

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
+#создание пустого слолваря
 username = {}
-
+#создание цикла while со значением True
 while True:
+    # ввод данных от пользователя
     name = input("Введите имя: ")
     age = input("Введите возраст: ")
-
+    # добавление введённых данных в словарь
     isName = username.get(name, None)
     if isName is None:
         username[name] = age
-
+    # запрос команды от пользователя
     command = input("Введите команду: ")
+    # проверка совпадения введённой команды с условием
     if command == "show":
-            for name in [username]:
-                print(username)
+        for name in [username]:
+            # вывод словаря с добавленного в него данными
+            print(username)
